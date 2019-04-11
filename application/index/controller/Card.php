fff6d8dba828986b36d79e3750a3c74451618b97
JHR <1873645345@qq.com>
2019-04-11 19:58:01 +0800

    保留备份

--- application/index/controller/Card.php
+++ application/index/controller/Card.php
@@ -341,7 +341,8 @@ class Card extends Common
         list($msec, $sec) = explode(' ', microtime());
         $msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
         $random = mt_rand(10000,99999);
-        $url = 'https://wx.gdhdtech.com/services/cardid2data/'.$cardid.'/'.$time.'/'.$count.'/'.$msectime.'/' . $random;
+//        $url = 'https://wx.gdhdtech.com/services/cardid2data/'.$cardid.'/'.$time.'/'.$count.'/'.$msectime.'/' . $random;
+        $url = 'http://115.29.228.5:8080/services/cardid2data/'.$cardid.'/'.$time.'/'.$count.'/'.$msectime.'/' . $random;
 
         $result = file_get_contents($url);
         $result_arr = json_decode($result,true);
